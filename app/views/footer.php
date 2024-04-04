a61afb2dad5a5dca141271b2b9beae64ad5fcc12
Mariia <705283@student.inholland.nl>
2024-04-04 23:23:39 +0200

    header and footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,41 +1,14 @@
-<style>
-    .footer {
-        background-color: #ececec;
-        padding: 10px 0;
-    }
-
-    .footer ul {
-        margin: 0;
-        padding: 0;
-    }
-
-    .footer .nav-item {
-        list-style: none;
-        display: inline-block;
-        margin-right: 10px;
-    }
-
-    .footer .nav-link {
-        color: #000000;
-        text-decoration: none;
-    }
-
-    .footer .nav-link:hover {
-        color: #177dfa;
-    }
-</style>
-
-
-<footer class="footer">
-    <div class="container">
+<footer class="footer pt-3 pb-3 bg-secondary-subtle">
+    <div>
         <ul class="nav justify-content-center">
-            <li class="nav-item"><a href="/" class="nav-link">Explore</a></li>
+            <li><a href="/" class="btn btn-link">Explore</a></li>
             <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) { ?>
-                <li class="nav-item"><a href="/userPage" class="nav-link">User Page</a></li>
-                <li class="nav-item"><a href="/login/logout" class="nav-link">Log Out</a></li>
+                <li><a href="/userPage" class="btn btn-link">User Page</a></li>
+                <li><a href="/createRecipe" class="btn btn-link">Create Recipe</a></li>
+                <li><a href="/login/logout" class="btn btn-link">Log Out</a></li>
             <?php } else { ?>
-                <li class="nav-item"><a href="/signup" class="nav-link">Sign Up</a></li>
-                <li class="nav-item"><a href="/login" class="nav-link">Login</a></li>
+                <li><a href="/signup" class="btn btn-link">Sign Up</a></li>
+                <li><a href="/login" class="btn btn-link">Login</a></li>
             <?php } ?>
         </ul>
     </div>
